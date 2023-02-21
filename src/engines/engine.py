c3b900144214e727bcdd97ae0fa794ac8d4f9c82
JunzheJosephZhu <junzhe.joseph.zhu@gmail.com>
2023-02-21 00:01:24 -0800

    delete extra files, clean up code

---
+++ src/engines/engine.py
@@ -0,0 +1,82 @@
+import time
+from pytorch_lightning import LightningModule
+import torch
+import torch.nn.functional as F
+from torch.autograd import Variable
+import numpy as np
+import torchvision
+import matplotlib.pyplot as plt
+import pandas as pd
+
+class ImiEngine(LightningModule):
+    def __init__(self, actor, optimizer, train_loader, val_loader, scheduler, config):
+        super().__init__()
+        self.actor = actor
+        self.optimizer = optimizer
+        self.train_loader = train_loader
+        self.val_loader = val_loader
+        self.scheduler = scheduler
+        self.config = config
+
+        self.loss_cce = torch.nn.CrossEntropyLoss()
+
+        self.wrong = 1
+        self.correct = 0
+        self.total = 0
+        self.save_hyperparameters(config)
+        print("baseline learn")
+
+    def compute_loss(self, demo, pred_logits, xyz_gt, xyz_pred):
+        immi_loss = self.loss_cce(pred_logits, demo)
+        aux_loss = F.mse_loss(xyz_gt, xyz_pred)
+        return immi_loss + aux_loss * self.config.aux_multiplier, immi_loss, aux_loss
+
+    def training_step(self, batch, batch_idx):
+        # use idx in batch for debugging
+        inputs, demo, xyzrpy_gt, optical_flow, start = batch
+        action_logits, xyzrpy_pred, weights = self.actor(inputs, start)  # , idx)
+        loss, immi_loss, aux_loss = self.compute_loss(demo, action_logits, xyzrpy_gt, xyzrpy_pred)
+        self.log_dict({"train/immi_loss": immi_loss, "train/aux_loss": aux_loss}, prog_bar=True)
+        action_pred = torch.argmax(action_logits, dim=1)
+        acc = (action_pred == demo).sum() / action_pred.numel()
+        self.log("train/acc", acc, on_step=True, on_epoch=True)
+        return loss
+
+
+
+    def validation_step(self, batch, batch_idx):
+        inputs, demo, xyzrpy_gt, optical_flow, start = batch  # , idx = batch
+        action_logits, xyzrpy_pred, weights = self.actor(inputs, start)  # , idx)
+        loss, immi_loss, aux_loss = self.compute_loss(demo, action_logits, xyzrpy_gt, xyzrpy_pred)
+        self.log_dict({"val/immi_loss": immi_loss, "val/aux_loss": aux_loss}, prog_bar=True)
+        action_pred = torch.argmax(action_logits, dim=1)
+        if weights != None and  batch_idx < 225:
+            weights = weights[0]
+            df_cm = pd.DataFrame(weights.cpu().numpy(), index = range(weights.shape[0]), columns=range(weights.shape[0]))
+            plt.figure(figsize = (10,7))
+            fig_ = sns.heatmap(df_cm, annot=True, cmap='Spectral').get_figure()
+            plt.close(fig_)
+            self.logger.experiment.add_figure("Confusion matrix", fig_, batch_idx)
+        # number of corrects and total number
+        return ((action_pred == demo).sum(), action_pred.numel())
+
+    def validation_epoch_end(self, validation_step_outputs):
+        numerator = 0
+        divider = 0
+        for (cor, total) in validation_step_outputs:
+            numerator += cor
+            divider += total
+        acc = numerator / divider
+        self.log("val/acc", acc, on_step=False, on_epoch=True)
+
+    def train_dataloader(self):
+        """Training dataloader"""
+        return self.train_loader
+
+    def val_dataloader(self):
+        """Validation dataloader"""
+        return self.val_loader
+
+    def configure_optimizers(self):
+        return [self.optimizer], [self.scheduler]
+
---
+++ train_imitation.py
@@ -0,0 +1,107 @@
+import torch
+from src.datasets.imi_dataset import ImitationEpisode
+from src.models.encoders import make_vision_encoder, make_tactile_encoder, make_audio_encoder,make_tactile_flow_encoder
+from src.models.imi_models import Actor
+from src.engines.engine import ImiEngine
+from torch.utils.data import DataLoader
+from src.train_utils import *
+import pandas as pd
+import numpy as np
+torch.multiprocessing.set_sharing_strategy("file_system")
+
+
+def strip_sd(state_dict, prefix):
+    """
+    strip prefix from state dictionary
+    """
+    return {k.lstrip(prefix): v for k, v in state_dict.items() if k.startswith(prefix)}
+
+
+def main(args):
+    train_csv = pd.read_csv(args.train_csv)
+    val_csv = pd.read_csv(args.val_csv)
+    if args.num_episode is None:
+        train_num_episode = len(train_csv)
+        val_num_episode = len(val_csv)
+    else:
+        train_num_episode = args.num_episode
+        val_num_episode = args.num_episode
+
+    train_set = torch.utils.data.ConcatDataset([ImitationEpisode(args.train_csv, args, i, args.data_folder) for i in range(train_num_episode)])
+    val_set = torch.utils.data.ConcatDataset([ImitationEpisode(args.val_csv, args, i, args.data_folder, False) for i in range(val_num_episode)])
+
+    # create weighted sampler to balance samples
+    train_label = []
+    for episode in train_set.datasets:
+        for idx in range(len(episode)):
+            train_label.append(episode.get_demo(idx))
+    # histogram of label
+    class_sample_count = np.zeros(pow(3, args.action_dim))
+    for t in np.unique(train_label):
+        class_sample_count[t] = len(np.where(train_label == t)[0])
+    weight = 1. / (class_sample_count + 1e-5)
+    samples_weight = np.array([weight[t] for t in train_label])
+    samples_weight = torch.from_numpy(samples_weight)
+    sampler = torch.utils.data.WeightedRandomSampler(samples_weight.type('torch.DoubleTensor'), len(samples_weight))
+
+    train_loader = DataLoader(train_set, args.batch_size, num_workers=8, sampler=sampler)
+    val_loader = DataLoader(val_set, 1, num_workers=8, shuffle=False)
+    
+    # v encoder
+    v_encoder = make_vision_encoder(args.encoder_dim)
+    # t encoder
+    t_encoder = make_tactile_encoder(args.encoder_dim)
+    # a encoder
+    a_encoder = make_audio_encoder(args.encoder_dim * args.num_stack, args.norm_audio)
+    
+    imi_model = Actor(v_encoder, t_encoder, a_encoder, args).cuda()
+    optimizer = torch.optim.Adam(imi_model.parameters(), lr=args.lr)
+    scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=args.period, gamma=args.gamma)
+    # save config
+    exp_dir = save_config(args)
+    # pl stuff
+    pl_module = ImiEngine(imi_model, optimizer, train_loader, val_loader, scheduler, args)
+    start_training(args, exp_dir, pl_module)
+
+if __name__ == "__main__":
+    import configargparse
+    p = configargparse.ArgParser()
+    p.add("-c", "--config", is_config_file=True, default="conf/imi/imi_learn.yaml")
+    p.add("--batch_size", default=32)
+    p.add("--lr", default=1e-4, type=float)
+    p.add("--gamma", default=0.9, type=float)
+    p.add("--period", default=3)
+    p.add("--epochs", default=65, type=int)
+    p.add("--resume", default=None)
+    p.add("--num_workers", default=8, type=int)
+    # imi_stuff
+    p.add("--conv_bottleneck", required=True, type=int)
+    p.add("--exp_name", required=True, type=str)
+    p.add("--encoder_dim", required=True, type=int)
+    p.add("--action_dim", default=3, type=int)
+    p.add("--num_stack", required=True, type=int)
+    p.add("--frameskip", required=True, type=int)
+    p.add("--use_mha", default=False, action="store_true")
+    # data
+    p.add("--train_csv", default="train.csv")
+    p.add("--val_csv", default="val.csv")
+    p.add("--data_folder", default="data/data_0502/test_recordings")
+    p.add("--resized_height_v", required=True, type=int)
+    p.add("--resized_width_v", required=True, type=int)
+    p.add("--resized_height_t", required=True, type=int)
+    p.add("--resized_width_t", required=True, type=int)
+    p.add("--num_episode", default=None, type=int)
+    p.add("--crop_percent", required=True, type=float)
+    p.add("--ablation", required=True)
+    p.add("--num_heads", required=True, type=int)
+    p.add("--use_flow", default=False, action="store_true")
+    p.add("--use_holebase", default=False, action="store_true")
+    p.add("--task", type=str)
+    p.add("--norm_audio", default=False, action="store_true")
+    p.add("--aux_multiplier", type=float)
+    p.add("--nocrop", default=False, action="store_true")
+
+    args = p.parse_args()
+    args.batch_size *= torch.cuda.device_count()
+    main(args)
+
